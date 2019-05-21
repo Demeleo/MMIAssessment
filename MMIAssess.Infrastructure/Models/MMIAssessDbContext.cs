7c946ff784581b3143582f8e3989cc4e2989425e
Desmond Davids <desmond@probia.co.za>
2019-05-22 01:03:55 +0200

    startup

---
+++ MMIAssess.Infrastructure/Models/MMIAssessDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MMIAssess.Infrastructure.Models
+{
+    public class MMIAssessDbContext: DbContext
+    {
+        public MMIAssessDbContext(DbContextOptions<MMIAssessDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
