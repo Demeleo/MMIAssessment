b09435c855b884b784f94f49c290e4a30fa52bf1
Desmond Davids <desmond@probia.co.za>
2019-05-26 16:43:10 +0200

    added nunit test before tyrone docker

---
+++ MMIAssess.Tests/Core/ConvertUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace MMIAssess.Tests.Core
+{
+    public class ConvertUnitTest
+    {
+        [Fact]
+        public void CorrectConvertTest()
+        {
+        }
+
+        [Fact]
+        public void IncompatibleConvertTest()
+        {
+        }
+
+        [Fact]
+        public void ConversionNotAvailableTest()
+        {
+        }
+    }
+}
